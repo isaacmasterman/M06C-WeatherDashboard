6344e579d97190f6e2624acc23aeec8e0db2ff35
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2023-10-16 16:59:20 +1100

    extracting icon code and displaying it from API

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,12 @@
-let city_name = "London"; // Replace with the city you want to search for
-const API_key = "95181cd738718c1439ace1f949449b4c"; // Replace with your actual API key
+document.addEventListener("DOMContentLoaded", function() {
+const API_key = "95181cd738718c1439ace1f949449b4c"; // Hide when deployed
 
-// Step 1: Get the Latitude and Longitude of the City
-const currentWeatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`;
+// Capture the city name from the search input and fetch weather data
+function handleCitySearch(cityName) {
+    const currentWeatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}`;
 
-fetch(currentWeatherEndpoint)
+    // Fetch weather data for the given city name
+    fetch(currentWeatherEndpoint)
     .then(response => response.json())
     .then(data => {
         const latitude = data.coord.lat;
@@ -15,6 +17,12 @@ fetch(currentWeatherEndpoint)
         document.querySelector(".temperature").textContent = `${data.main.temp}°C`;
         document.querySelector(".wind-speed").textContent = `${data.wind.speed} m/s`;
         document.querySelector(".humidity").textContent = `${data.main.humidity}%`;
+
+        // Extract the icon code and update the image src for current weather
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+        console.log(document.querySelector("#weather-icon"));
+        document.querySelector("#weather-icon").src = iconUrl;
 
         // Step 2: Use the Latitude and Longitude to Fetch the 5-Day Forecast
         const forecastEndpoint = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_key}`;
@@ -38,6 +46,11 @@ fetch(currentWeatherEndpoint)
                 forecastSections[dayCount].querySelector(".temperature").textContent = `${forecast.main.temp}°C`;
                 forecastSections[dayCount].querySelector(".wind-speed").textContent = `${forecast.wind.speed} m/s`;
                 forecastSections[dayCount].querySelector(".humidity").textContent = `${forecast.main.humidity}%`;
+
+                // Extract the icon code and update the image src for the forecast
+                const forecastIconCode = forecast.weather[0].icon;
+                const forecastIconUrl = `https://openweathermap.org/img/wn/${forecastIconCode}.png`;
+                forecastSections[dayCount].querySelector("img").src = forecastIconUrl;
     
                 dayCount++; // Increment the day counter
             }
@@ -46,3 +59,46 @@ fetch(currentWeatherEndpoint)
     .catch(error => {
         console.error("Error fetching data:", error);
     });
+
+    // Save the city name to localStorage
+    let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    if (!searchHistory.includes(cityName)) { // Avoid duplicates
+        searchHistory.push(cityName);
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+    }
+
+    // Update the search history display
+    displaySearchHistory();
+}
+
+function displaySearchHistory() {
+    const searchHistoryList = document.querySelector('#searchInput .list-group');
+    searchHistoryList.innerHTML = ''; // Clear the current list
+
+    let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    searchHistory.forEach(city => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-group-item', 'rounded');
+        listItem.textContent = city;
+        listItem.addEventListener('click', function() {
+            handleCitySearch(city);
+        });
+        searchHistoryList.appendChild(listItem);
+    });
+}
+
+// Event listener for the search button
+document.querySelector('#searchInput button').addEventListener('click', function() {
+    const cityName = document.querySelector('#searchInput input').value.trim();
+    if (cityName) {
+        handleCitySearch(cityName);
+    }
+});
+
+// Initial display of search history when the page loads
+displaySearchHistory();
+
+// Fetch and display weather data for the default city
+handleCitySearch("Sydney");
+
+});
